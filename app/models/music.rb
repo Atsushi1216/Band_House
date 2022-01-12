0f679aa60bbc60ad2ffce93e9182d2b1a2c5c3a7
Atsushi1216 <ats1216@gmail.com>
2022-01-12 13:04:26 +0000

    [Add]評価機能

--- app/models/music.rb
+++ app/models/music.rb
@@ -25,4 +25,8 @@ mount_uploader :song, AudiofileUploader
     favorites.where(user_id: user.id).exists?
   end
 
+  validates :evaluation, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }
 end
