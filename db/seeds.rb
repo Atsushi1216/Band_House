55dafa4ed54c238cb0669b1dbc664a8383937624
Atsushi1216 <ats1216@gmail.com>
2022-01-15 05:53:47 +0000

    [fix]devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #User情報
-User.create!(:email => "aaa@gmail.com",:name => "AHDGs",:profile_image => File.open('./app/assets/images/AHDGs.jpg'),:encrypted_password => "000000")
-User.create!(:email => "bbb@gmail.com",:name => "TenderTemper",:profile_image => File.open('./app/assets/images/tendertemper.jpg'),:encrypted_password => "000000")
-User.create!(:email => "ccc@gmail.com",:name => "Yudai.K",:profile_image => File.open('./app/assets/images/Yudai.png'),:encrypted_password => "000000")
-User.create!(:email => "ddd@gmail.com",:name => "匿さん",:profile_image => File.open('./app/assets/images/AHDGs3.jpg'),:encrypted_password => "000000")
+User.create!(:email => "aaa@gmail.com",:name => "AHDGs",:profile_image => File.open('./app/assets/images/AHDGs.jpg'),:password => "000000")
+User.create!(:email => "bbb@gmail.com",:name => "TenderTemper",:profile_image => File.open('./app/assets/images/tendertemper.jpg'),:password => "000000")
+User.create!(:email => "ccc@gmail.com",:name => "Yudai.K",:profile_image => File.open('./app/assets/images/Yudai.png'),:password => "000000")
+User.create!(:email => "ddd@gmail.com",:name => "匿さん",:profile_image => File.open('./app/assets/images/AHDGs3.jpg'),:password => "000000")
