80c47a2fd647ffb81751a42c6aa0138bbef43a90
Atsushi1216 <ats1216@gmail.com>
2022-01-28 04:44:12 +0000

    [Update]レイアウト

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def send_mail(contact)
     @contact = contact
-
+    #ENVファイルで指定した'TOMAIL'へ送信
     mail to: ENV['TOMAIL'], subject: "メールのタイトル"
   end
 end
