ba8d0a07d22b1f89728f66a70544807c0c5880a6
Atsushi1216 <ats1216@gmail.com>
2022-02-09 12:11:39 +0000

    [Add]validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true
   validates :email, presence: true, length: {maximum:255},
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :message, presence: true
